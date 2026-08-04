9c54fe45200c06f61ef9d6176ba0190f22f010db
Nitin Yadav <nyadav4625@gmail.com>
2026-08-05 02:20:56 +0530

    Excercise 13

--- blog/app/actions/blogs.ts
+++ blog/app/actions/blogs.ts
@@ -6,14 +6,23 @@ import { revalidatePath } from "next/cache"
 import { auth } from "@/auth"
 
 
-export const createBlog = async(formData: FormData) => {
+export const createBlog = async(prevState: {error: string}, formData: FormData) => {
     const session = await auth()
   if (!session) {
     redirect("/login")
   }
     const title = formData.get("title") as string
+    if (!title || title.length < 5) {
+        return {error: "title must be at least 5 characters long"}
+    }
     const author = formData.get("author") as string
+    if (!author || author.length < 5) {
+        return {error: "author name must be at least 5 characters long"}
+    }
     const url = formData.get("url") as string
+    if (!url || url.length < 5) {
+        return {error: "url must be at least 5 characters long"}
+    }
 
     await addBlogs(title, author, url)
     revalidatePath("/blogs")
--- blog/app/blogs/new/page.tsx
+++ blog/app/blogs/new/page.tsx
@@ -1,21 +1,25 @@
+"use client"
 import { createBlog } from "@/app/actions/blogs"
+import { useActionState } from "react"
 const newBlog = () => {
+    const [state, formAction] = useActionState(createBlog, {error: ""})
     return (
         <div>
-            <form action={createBlog}>
+            <form action={formAction}>
                 <label>
                     Title
-                    <input type="text"  name="title" required/>
+                    <input type="text"  name="title" />
                 </label>
                 <label>
                     Author
-                    <input type="text"  name="author" required/>
+                    <input type="text"  name="author" />
                 </label>
                 <label>
                     Url
-                    <input type="text"  name="url" required/>
+                    <input type="text"  name="url" />
                 </label>
                 <button type="submit">Submit</button>
+                {state.error && <p style={{ color: "red" }}>{state.error}</p>}
             </form>
         </div>
     )
