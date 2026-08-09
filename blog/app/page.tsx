2ac07214b426d48616a45473ffae77e383f4319e
Nitin Yadav <nyadav4625@gmail.com>
2026-08-09 18:21:15 +0530

    Exercise 22

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+"use client"
+
+import Homepage from "./homepage.mdx"
+
+const Home = () => {
   return (
-    <div>
-      
+    <div className="max-w-4xl mx-auto p-6 markdown">
+      <Homepage />
     </div>
-  );
+  )
 }
+
+export default Home
